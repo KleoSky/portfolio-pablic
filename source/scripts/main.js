fe4a093612eb2da65b343c9107172f401033444c
KleoSky <V-Kleo@yandex.ru>
2025-12-26 00:23:41 +0700

    Добавляет десктопную версию

---
+++ source/scripts/main.js
@@ -0,0 +1,27 @@
+// import { menuOpen, subMenuOpen } from './modules/header';
+// menuOpen();
+// subMenuOpen();
+
+// import { initSwiperHero } from './modules/hero';
+// initSwiperHero();
+
+// import { initFormValidation } from './modules/form-validate';
+// initFormValidation();
+
+// import { modalOpen } from './modules/modal';
+// modalOpen();
+
+// import { initSelectArrow } from './modules/select';
+// initSelectArrow();
+
+// import { initSwiperPrograms } from './modules/programs';
+// initSwiperPrograms();
+
+// import { initSwiperNews } from './modules/news';
+// initSwiperNews();
+
+// import { initAccordion } from './modules/faq';
+// initAccordion();
+
+// import { initSwiperRewiews } from './modules/reviews';
+// initSwiperRewiews();
